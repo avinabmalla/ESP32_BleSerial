82a15f73a1cc1168b6c653b835ac8d46ad0d2cf3
Avinab Malla <avinabmalla@yahoo.com>
2021-05-15 20:27:09 +0545

    Added files

---
+++ Example/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <BleSerial.h>
+
+BleSerial ble;
+
+void setup()
+{
+	//Start the BLE Serial
+	//Enter the Bluetooth name here
+	ble.begin("BleSerialTest");
+}
+
+void loop()
+{
+	//The usage is similar to Serial
+	ble.println("Hello!");
+	delay(1000);
+}
